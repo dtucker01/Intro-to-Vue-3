a114fe28288b1d86a993406c47888edd315410cd
dtucker01 <dennis@tuckersemail.com>
2020-11-03 13:36:23 -0700

    Initial Chapter 3 commit

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            productDescription: 'Blue Socks!'
+            product: 'Socks'
         }
     }
 })
