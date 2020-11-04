0adf9c5f7f8ce9b439ed55b6c3dbb989e4f71638
dtucker01 <dennis@tuckersemail.com>
2020-11-04 12:10:28 -0700

    Chapter 5. List Rendering

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks', 
             image: './assets/images/socks_blue.jpg', 
-            inventory: 8,
-            onSale: false
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20%'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue' },
+            ],
+            sizes: [ 'S','M', 'L', 'XL']
         }
     }
 })
